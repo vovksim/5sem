02ec9573da8ad44dfa9131ddbee2914fb757a1c7
vovksim <ivan.mykhailov11@gmail.com>
2024-10-20 18:14:55 +0300

    Organizing files.

--- DistributedCalculations/randomDataGenerator.cpp
+++
@@ -1,8 +0,0 @@
-#include <random>
-
-
-class dataGenerator {
-    static std::random_device rd; // a seed source for the random number engine
-    static std::mt19937 gen(rd()); // mersenne_twister_engine seeded with rd()
-    static std::uniform_real_distribution<> distrib(0,RAND_MAX);
-}
