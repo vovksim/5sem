c18074de10b12ce1743af7c848054f7889be795e
vovksim <ivan.mykhailov11@gmail.com>
2024-10-26 14:34:01 +0300

    Minor changes.

--- Operational Systems/Lab2/lottery.py
+++ Operational Systems/Lab2/lottery.py
@@ -1,7 +1,5 @@
-#! /usr/bin/env python3
-
+#! /usr/bin/env python
 from __future__ import print_function
-import sys
 from optparse import OptionParser
 import random
 
@@ -16,150 +14,133 @@ def random_seed(seed):
 
 
 parser = OptionParser()
-parser.add_option("-s", "--seed", default=0, help="the random seed", action="store", type="int", dest="seed")
-parser.add_option("-j", "--jobs", default=3, help="number of jobs in the system", action="store", type="int",
-                  dest="jobs")
-parser.add_option("-l", "--jlist", default="",
-                  help="instead of random jobs, provide a comma-separated list of run times", action="store",
-                  type="string", dest="jlist")
-parser.add_option("-m", "--maxlen", default=10, help="max length of job", action="store", type="int", dest="maxlen")
-parser.add_option("-p", "--policy", default="FIFO", help="sched policy to use: SJF, FIFO, RR", action="store",
-                  type="string", dest="policy")
-parser.add_option("-q", "--quantum", help="length of time slice for RR policy", default=1, action="store", type="int",
-                  dest="quantum")
-parser.add_option("-c", help="compute answers for me", action="store_true", default=False, dest="solve")
+parser.add_option('-s', '--seed', default=0, help='the random seed', action='store', type='int', dest='seed')
+parser.add_option('-j', '--jobs', default=3, help='number of jobs in the system', action='store', type='int',
+                  dest='jobs')
+parser.add_option('-l', '--jlist', default='',
+                  help='instead of random jobs, provide a comma-separated list of run times and ticket values (e.g., 10:100,20:100 would have two jobs with run-times of 10 and 20, each with 100 tickets)',
+                  action='store', type='string', dest='jlist')
+parser.add_option('-m', '--maxlen', default=10, help='max length of job', action='store', type='int', dest='maxlen')
+parser.add_option('-T', '--maxticket', default=100, help='maximum ticket value, if randomly assigned', action='store',
+                  type='int', dest='maxticket')
+parser.add_option('-q', '--quantum', default=1, help='length of time slice', action='store', type='int', dest='quantum')
+parser.add_option('-c', '--compute', help='compute answers for me', action='store_true', default=False, dest='solve')
+parser.add_option('-d', '--diff', help='log difference between job endings', action='store_true', default=False,
+                  dest='diff')
 
 (options, args) = parser.parse_args()
 
 random_seed(options.seed)
 
-print('ARG policy', options.policy)
-if options.jlist == '':
-    print('ARG jobs', options.jobs)
-    print('ARG maxlen', options.maxlen)
-    print('ARG seed', options.seed)
-else:
-    print('ARG jlist', options.jlist)
+print('ARG jlist', options.jlist)
+print('ARG jobs', options.jobs)
+print('ARG maxlen', options.maxlen)
+print('ARG maxticket', options.maxticket)
+print('ARG quantum', options.quantum)
+print('ARG seed', options.seed)
+print('ARG diff', options.diff)
 print('')
 
 print('Here is the job list, with the run time of each job: ')
 
 import operator
 
+tickTotal = 0
+runTotal = 0
 joblist = []
+
 if options.jlist == '':
     for jobnum in range(0, options.jobs):
-        runtime = int(options.maxlen * random.random()) + 1
-        joblist.append([jobnum, runtime])
-        print('  Job', jobnum, '( length = ' + str(runtime) + ' )')
+        runtime = 0
+        while runtime == 0:
+            runtime = int(options.maxlen * random.random())
+        tickets = 0
+        while tickets == 0:
+            tickets = int(options.maxticket * random.random())
+        runTotal += runtime
+        tickTotal += tickets
+        joblist.append([jobnum, runtime, tickets])
+        print('  Job %d ( length = %d, tickets = %d )' % (jobnum, runtime, tickets))
 else:
     jobnum = 0
-    for runtime in options.jlist.split(','):
-        joblist.append([jobnum, float(runtime)])
+    for entry in options.jlist.split(','):
+        (runtime, tickets) = entry.split(':')
+        joblist.append([jobnum, int(runtime), int(tickets)])
+        runTotal += int(runtime)
+        tickTotal += int(tickets)
         jobnum += 1
     for job in joblist:
-        print('  Job', job[0], '( length = ' + str(job[1]) + ' )')
+        print('  Job %d ( length = %d, tickets = %d )' % (job[0], job[1], job[2]))
 print('\n')
+
+if len(joblist) == 2 and options.diff:
+    endTimeList = []
+elif options.diff == True and len(joblist) != 2:
+    raise ValueError("Available only for 2 processes.")
+
+if options.solve == False:
+    print('Here is the set of random numbers you will need (at most):')
+    for i in range(runTotal):
+        r = int(random.random() * 1000001)
+        print('Random', r)
 
 if options.solve == True:
     print('** Solutions **\n')
-    if options.policy == 'SJF':
-        joblist = sorted(joblist, key=operator.itemgetter(1))
-        options.policy = 'FIFO'
 
-    if options.policy == 'FIFO':
-        thetime = 0
-        print('Execution trace:')
-        for job in joblist:
-            print('  [ time %3d ] Run job %d for %.2f secs ( DONE at %.2f )' % (
-            thetime, job[0], job[1], thetime + job[1]))
-            thetime += job[1]
+    jobs = len(joblist)
+    clock = 0
+    for i in range(runTotal):
+        r = int(random.random() * 1000001)
+        winner = int(r % tickTotal)
 
-        print('\nFinal statistics:')
-        t = 0.0
-        count = 0
-        turnaroundSum = 0.0
-        waitSum = 0.0
-        responseSum = 0.0
-        for tmp in joblist:
-            jobnum = tmp[0]
-            runtime = tmp[1]
+        current = 0
+        for (job, runtime, tickets) in joblist:
+            current += tickets
+            if current > winner:
+                (wjob, wrun, wtix) = (job, runtime, tickets)
+                break
 
-            response = t
-            turnaround = t + runtime
-            wait = t
-            print('  Job %3d -- Response: %3.2f  Turnaround %3.2f  Wait %3.2f' % (jobnum, response, turnaround, wait))
-            responseSum += response
-            turnaroundSum += turnaround
-            waitSum += wait
-            t += runtime
-            count = count + 1
-        print('\n  Average -- Response: %3.2f  Turnaround %3.2f  Wait %3.2f\n' % (
-        responseSum / count, turnaroundSum / count, waitSum / count))
+        print('Random', r, '-> Winning ticket %d (of %d) -> Run %d' % (winner, tickTotal, wjob))
+        # print('Winning ticket %d (of %d) -> Run %d' % (winner, tickTotal, wjob))
 
-    if options.policy == 'RR':
-        print('Execution trace:')
-        turnaround = {}
-        response = {}
-        lastran = {}
-        wait = {}
-        quantum = float(options.quantum)
-        jobcount = len(joblist)
-        for i in range(0, jobcount):
-            lastran[i] = 0.0
-            wait[i] = 0.0
-            turnaround[i] = 0.0
-            response[i] = -1
-
-        runlist = []
-        for e in joblist:
-            runlist.append(e)
-
-        thetime = 0.0
-        while jobcount > 0:
-            job = runlist.pop(0)
-            jobnum = job[0]
-            runtime = float(job[1])
-            if response[jobnum] == -1:
-                response[jobnum] = thetime
-            currwait = thetime - lastran[jobnum]
-            wait[jobnum] += currwait
-            if runtime > quantum:
-                runtime -= quantum
-                ranfor = quantum
-                print('  [ time %3d ] Run job %3d for %.2f secs' % (thetime, jobnum, ranfor))
-                runlist.append([jobnum, runtime])
+        print('  Jobs:', )
+        for (job, runtime, tickets) in joblist:
+            if wjob == job:
+                wstr = '*'
             else:
-                ranfor = runtime;
-                print('  [ time %3d ] Run job %3d for %.2f secs ( DONE at %.2f )' % (
-                thetime, jobnum, ranfor, thetime + ranfor))
-                turnaround[jobnum] = thetime + ranfor
-                jobcount -= 1
-            thetime += ranfor
-            lastran[jobnum] = thetime
+                wstr = ' '
 
-        print('\nFinal statistics:')
-        turnaroundSum = 0.0
-        waitSum = 0.0
-        responseSum = 0.0
-        for i in range(0, len(joblist)):
-            turnaroundSum += turnaround[i]
-            responseSum += response[i]
-            waitSum += wait[i]
-            print(
-                '  Job %3d -- Response: %3.2f  Turnaround %3.2f  Wait %3.2f' % (i, response[i], turnaround[i], wait[i]))
-        count = len(joblist)
+            if runtime > 0:
+                tstr = tickets
+            else:
+                tstr = '---'
+            print(' (%s job:%d timeleft:%d tix:%s ) ' % (wstr, job, runtime, tstr), end='')
+        print('')
+        # now do the accounting
+        if wrun >= options.quantum:
+            wrun -= options.quantum
+        else:
+            wrun = 0
 
-        print('\n  Average -- Response: %3.2f  Turnaround %3.2f  Wait %3.2f\n' % (
-        responseSum / count, turnaroundSum / count, waitSum / count))
+        clock += options.quantum
 
-    if options.policy != 'FIFO' and options.policy != 'SJF' and options.policy != 'RR':
-        print('Error: Policy', options.policy, 'is not available.')
-        sys.exit(0)
-else:
-    print('Compute the turnaround time, response time, and wait time for each job.')
-    print('When you are done, run this program again, with the same arguments,')
-    print('but with -c, which will thus provide you with the answers. You can use')
-    print('-s <somenumber> or your own job list (-l 10,15,20 for example)')
-    print('to generate different problems for yourself.')
-    print('')
+        # job completed!
+        if wrun == 0:
+            if options.diff:
+                endTimeList.append(clock)
+            print('--> JOB %d DONE at time %d' % (wjob, clock))
+            tickTotal -= wtix
+            wtix = 0
+            jobs -= 1
+
+        # update job list
+        joblist[wjob] = (wjob, wrun, wtix)
+
+        if jobs == 0:
+            if options.diff:
+                with open("stat", "a") as f:
+                    f.write(str(abs(endTimeList[1] - endTimeList[0])) + " ")
+                # Fairness metrics as in book Job1FinishTime/Job2FinishTime
+                # f.write(str(abs(endTimeList[0]/endTimeList[1]))+" ")
+            print('')
+            break
